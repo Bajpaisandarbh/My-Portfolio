b67fe6f4256734234e1baa84f5beb5fbae325903
Sandarbh Bajpai <118759857+Bajpaisandarbh@users.noreply.github.com>
2024-06-25 16:34:06 +0530

    Delete routes directory

--- routes/PortfolioRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { sendEmailController } = require("../controllers/PortFolioController");
-const router = express.Router();
-
-router.post("/sendEmail", sendEmailController);
-
-module.exports = router;
